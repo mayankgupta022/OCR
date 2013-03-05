a6c5df37580e64c606a615523e24dfea14331cb0
Mayank Gupta <genee26@gmail.com>
2013-03-06 02:57:22 +0530

    default
    default commit with basic implementation

---
+++ src/example.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import net.sourceforge.tess4j.*;
+
+public class example {
+
+    public static void main(String[] args) {
+        File imageFile = new File("eurotext.tif");
+        Tesseract instance = Tesseract.getInstance(); // JNA Interface Mapping
+        // Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
